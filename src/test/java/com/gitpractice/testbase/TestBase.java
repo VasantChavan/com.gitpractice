e61443eefb2c93311625aaed2616848e97ad1703
Vasant Chavan <vasant.chavan223@gmail.com>
2024-04-20 10:28:26 +0530

    added few statement in testbase.java

--- src/test/java/com/gitpractice/testbase/TestBase.java
+++ src/test/java/com/gitpractice/testbase/TestBase.java
@@ -2,9 +2,15 @@ package com.gitpractice.testbase;
 
 public class TestBase {
 
+	public TestBase()
+	{
+		System.out.println("TestBase const");
+	}
 
   public static void main(String[] args){
 
     System.out.println("Hello team");
+    
+    new TestBase();
   }
 }
